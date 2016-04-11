0e5a2b71762b76db24d9ee836999ddf345a75ecb
lvsh2012 <lvsh2012@gmail.com>
2016-04-12 02:26:52 +0800

    douban demo

--- content_scripts.js
+++ content_scripts.js
@@ -16,32 +16,49 @@ function init(){
     urlcheck();
 }
 
-function res(movieid){
-     var movieResHtml = "<div class='muwei3-movie-res'>\
-                       <h3 class='online-title'>在线观看</h3>\
-                       <div class='muwei3-movie-online'></div>\
-                       <h3 class='download-title'>资源下载</h3>\
-                       <div class='muwei3-movie-download'></div>\
+function httpRequestGet(callback,url){
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function(data) {
+		if (xhr.readyState == 4) {
+			if (xhr.status == 200) {
+				var data = JSON.parse(xhr.responseText);
+				callback(data);
+			} else {
+				callback(null);
+			}
+		}
+	}
+	xhr.open('GET', url, true);
+	xhr.send();	
+}
+
+function resShow(json){
+    if(json.status == 0){
+	
+		var onlineHtml = "";
+		if(!$.isEmptyObject(json.data.online)){
+			onlineHtml = "<h3 class='online-title'>在线观看</h3><div class='muwei3-movie-online'></div>";
+		}
+		downloadHtml = "";
+		if(!$.isEmptyObject(json.data.download)){
+			var downloadHtml = "<h3 class='download-title'>资源下载</h3><div class='muwei3-movie-download'></div>";
+		}
+		var movieResHtml = "<div class='muwei3-movie-res'>"+onlineHtml+downloadHtml+"\
+					   <div class='muwei3-right'><a href='http:www.muwei3.com' target='_blank'>木卫三</a></div>\
                        </div>";
-    $(".aside").prepend(movieResHtml);
+		$(".aside").prepend(movieResHtml);
+        $.each(json.data.online,function(k,v){
+            $(".muwei3-movie-online").append("<div><a href='"+v.url+"' target='_blank'>"+v.title+"</a></div>")
+        })
+        $.each(json.data.download,function(k,v){
+            $(".muwei3-movie-download").append("<div><a href='"+v.url+"' target='_blank'>"+v.title+"</a></div>")
+        })
+    }
+}
 
-    $.ajax({
-             url:conf.movie_res+movieid,
-             dataType:"jsonp",
-             jsonp:"jsonpcallback",
-             jsonpCallback:'callback',
-             success:function(json){
-              if(json.status == 0){
-                $.each(json.data.online,function(k,v){
-                  $(".muwei3-movie-online").append("<a href='"+v.url+"'>"+v.title+"</a>")
-                })
-                $.each(json.data.download,function(k,v){
-                  $(".muwei3-movie-download").append("<a href='"+v.url+"'>"+v.title+"</a>")
-                })
-              }
-             }
-        });
-
+function res(movieid){
+	var url = conf.movie_res+movieid;
+	httpRequestGet(resShow,url)
 }
 
 function opt(){
