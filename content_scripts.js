a2cf8d76ed5f6aaaae97bd9f933fe98850042756
lvshe2012@gmail.com <lvsh2012@gmail.com>
2016-04-11 17:58:04 +0800

    11

--- content_scripts.js
+++ content_scripts.js
@@ -1,20 +1,22 @@
 var conf = {
-    movie_res:'http://movie.muwei3.com/douban/res/fetch',
-    movie_opt:'http://movie.muwei3.com/douban/res/opt'
+    movie_res:'https://task.muwei3.com/index.php/movie/show/res/',
+    movie_opt:'https://task.muwei3.com/index.php/douban/res/opt'
 }
 
 function urlcheck(){
     var url = window.location.href;
     var reg = /movie\.douban\.com\/subject\/\d+/;
     if(reg.test(url)){
-         res();
+        var result = url.match(/subject\/\d+/);
+        var movieid = result[0].replace("subject/", "");
+        res(movieid);
     }
 }
 function init(){
     urlcheck();
 }
 
-function res(){
+function res(movieid){
      var movieResHtml = "<div class='muwei3-movie-res'>\
                        <h3 class='online-title'>在线观看</h3>\
                        <div class='muwei3-movie-online'></div>\
@@ -22,9 +24,24 @@ function res(){
                        <div class='muwei3-movie-download'></div>\
                        </div>";
     $(".aside").prepend(movieResHtml);
-    $.post(conf.movie_res,function(json){
-        
-    },'json');
+
+    $.ajax({
+             url:conf.movie_res+movieid,
+             dataType:"jsonp",
+             jsonp:"jsonpcallback",
+             jsonpCallback:'callback',
+             success:function(json){
+              if(json.status == 0){
+                $.each(json.data.online,function(k,v){
+                  $(".muwei3-movie-online").append("<a href='"+v.url+"'>"+v.title+"</a>")
+                })
+                $.each(json.data.download,function(k,v){
+                  $(".muwei3-movie-download").append("<a href='"+v.url+"'>"+v.title+"</a>")
+                })
+              }
+             }
+        });
+
 }
 
 function opt(){
